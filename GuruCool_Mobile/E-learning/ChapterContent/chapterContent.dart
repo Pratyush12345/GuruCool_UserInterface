4f0f42b2f9cd6c6d588252f9ad46567a5ce1f849
Raj Baindra <baindraraj18@gmail.com>
2020-12-16 15:03:33 +0530

    removed main,MyApp and SizeConfig

--- GuruCool_Mobile/E-learning/ChapterContent/chapterContent.dart
+++ GuruCool_Mobile/E-learning/ChapterContent/chapterContent.dart
@@ -4,41 +4,9 @@ import 'package:qwe/utils/Colors.dart';
 import 'package:qwe/utils/bottom.dart';
 import 'package:qwe/ChapterContent/speedDialStf.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-void main() {
-runApp(MyApp());
-}
+import '../../utils/sizeConfig.dart';
 
-class SizeConfig {
-MediaQueryData _mediaQueryData;
-static double screenWidth;
-static double screenHeight;
-static double _safeAreaHorizontal;
-static double _safeAreaVertical;
-static double b;
-static double v;
 
-void init(BuildContext context) {
-_mediaQueryData = MediaQuery.of(context);
-screenHeight = _mediaQueryData.size.height;
-screenWidth = _mediaQueryData.size.width;
-_safeAreaHorizontal =_mediaQueryData.padding.left + _mediaQueryData.padding.right;
-_safeAreaVertical =_mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
-b = (screenWidth - _safeAreaHorizontal) / 100;
-v = (screenHeight - _safeAreaVertical) / 100;
-}
-}
-
-class MyApp extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-debugShowCheckedModeBanner: false,
-theme: ThemeData(
-visualDensity: VisualDensity.adaptivePlatformDensity,
-),
-home: ChapterContent(subjectName:"English"));
-}
-}
 class ChapterContent extends StatelessWidget {
 final String courseId;
 final String contentType;
@@ -65,11 +33,15 @@ icon: Icon(Icons.menu,color: Color(0xffF36C24),),
 onPressed: () {
 }),
 title:Transform(transform: Matrix4.translationValues(-SizeConfig.screenWidth*0.06, 0, 0),
-child:Text(this.subjectName,style: TextStyle(color: gc, fontWeight: FontWeight.w400,
-fontSize:SizeConfig.screenWidth*0.05))),
+child:Text(this.subjectName,
+           style: TextStyle(
+             color: gc,
+             fontWeight: FontWeight.w400,
+             fontSize:SizeConfig.screenWidth*0.05))),
 actions: [
 Row(children:[
-IconButton(icon: Icon(Icons.notifications_none,color: gc),
+IconButton(
+  icon: Icon(Icons.notifications_none,color: gc),
 onPressed: () {
 }),
 CircleAvatar(backgroundColor:Color(0xffA4A4A4),
@@ -115,14 +87,18 @@ decoration:BoxDecoration(
 borderRadius: BorderRadius.only(topLeft:Radius.circular(SizeConfig.b * 2),
 bottomLeft:Radius.circular(SizeConfig.b * 2) ),
 color:index%3==0?gc:(index%3==1?bt:rc)),  
-child:Icon(index%3==0?MdiIcons.clock:(index%3==1?Icons.picture_as_pdf:MdiIcons.youtube),color:Colors.white),
+child:Icon(
+  index%3==0?MdiIcons.clock:(index%3==1?Icons.picture_as_pdf:MdiIcons.youtube),
+  color:Colors.white),
 ),
 SizedBox(width:SizeConfig.screenWidth*0.089),
 Container(
 width:SizeConfig.screenWidth*0.67,  
 child:Text(index%3==0?"Quiz":(index%3==1?"Topic of the PDF Topic of the PDF Topic of the PDF":"Video Lecture"),
 //to define content type and its heading 
-style:TextStyle(color:Colors.black,fontSize:SizeConfig.screenWidth*0.045)))
+style:TextStyle(
+  color:Colors.black,
+  fontSize:SizeConfig.screenWidth*0.045)))
 ]
 )
 ));
